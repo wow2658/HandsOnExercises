61abab9fc78798823016c6c286cc6f9fb9a353ca
Vp_pDoochi <wow2658@naver.com>
2024-03-24 23:54:40 +0900

    Create Head Recursion Tail Recursion.cpp

---
+++ Recursion/Head Recursion Tail Recursion.cpp
@@ -0,0 +1,41 @@
+#if 0
+// Head Recursion
+#include <stdio.h>
+void fun(int n)
+{
+	if (n > 0)
+	{
+		fun(n - 1);
+		printf("%d ", n);
+
+	}
+}
+int main()
+{
+	int x = 3;
+
+	fun(x);
+	return 0;
+}
+#endif
+
+#if 1
+// Tail Recursion
+#include <stdio.h>
+void fun(int n)
+{
+	if (n > 0)
+	{
+		printf("%d ", n);
+		fun(n - 1);
+
+	}
+}
+int main()
+{
+	int x = 3;
+
+	fun(x);
+	return	0;
+}
+#endif
